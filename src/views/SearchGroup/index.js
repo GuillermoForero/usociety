734648b17cee524df207418d7e2eacefc1a02689
Guillermo <guillermoforerosuarez@gmail.com>
2021-01-30 11:24:45 -0500

    .

--- src/views/SearchGroup/index.js
+++ src/views/SearchGroup/index.js
@@ -27,7 +27,7 @@ import {withStyles} from "@material-ui/core/styles";
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
-        backgroundColor: 'gray',
+        backgroundColor: 'var(--terciary)',
         color: theme.palette.common.white,
     },
     body: {

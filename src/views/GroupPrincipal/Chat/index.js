d07796f35cf5d5b2236430b182260c0e77948916
Ardila Javier <javier.ardila@flexibility.com.ar>
2021-01-30 10:39:08 -0500

    Optimized imports.

--- src/views/GroupPrincipal/Chat/index.js
+++ src/views/GroupPrincipal/Chat/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
 import Fab from '@material-ui/core/Fab';
 import SendIcon from '@material-ui/icons/Send';
-import {AppBar, Button, IconButton, Menu, MenuItem, Toolbar} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import {AccountCircle} from "@material-ui/icons";
+import {Button} from "@material-ui/core";
 
 const useStyles = makeStyles({
     table: {
